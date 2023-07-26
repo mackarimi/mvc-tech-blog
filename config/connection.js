2043a8a5e1122101059a254658ed92e53d8d9c8a
mackarimi <kkarimi47@gmail.com>
2023-07-26 16:06:40 -0700

    final touch

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,22 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-// Create connection to the database using JAWSDB_URL if available
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: process.env.DB_HOST || "localhost",
-      port: process.env.DB_PORT || 3306,
-      dialect: "mysql",
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  // Use JAWSDB_URL for database connection if available (on Heroku)
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  // Use local database configuration for development
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT || 3306,
+    dialect: "mysql",
+    dialectOptions: {
+      decimalNumbers: true,
+    },
+  });
+}
 
 async function connect() {
   try {
