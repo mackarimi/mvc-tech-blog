06b6031efeb5c233b18a474555d6d65020c7c122
mackarimi <kkarimi47@gmail.com>
2023-07-26 16:01:59 -0700

    final touch

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,7 @@
-// Import the Sequelize constructor from the library
 const Sequelize = require("sequelize");
-
 require("dotenv").config();
 
-// Create connection to our database, pass in your MySQL information for username and password
+// Create connection to the database using JAWSDB_URL if available
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
