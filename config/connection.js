a03d57d234d7f73c2eea72bb66dca5c2d8a6af4b
mackarimi <kkarimi47@gmail.com>
2023-07-26 16:08:51 -0700

    fix database connection

--- config/connection.js
+++ config/connection.js
@@ -1,32 +1,14 @@
-const Sequelize = require("sequelize");
-require("dotenv").config();
+const { Sequelize } = require('sequelize');
+require('dotenv').config(); 
 
-let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  // Use JAWSDB_URL for database connection if available (on Heroku)
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  // Use local database configuration for development
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: process.env.DB_HOST || "localhost",
-    port: process.env.DB_PORT || 3306,
-    dialect: "mysql",
-    dialectOptions: {
-      decimalNumbers: true,
-    },
-  });
-}
+const sequelize = process.env.JAWSDB_URL 
+  ? new Sequelize(process.env.JAWSDB_URL) 
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, { 
+      host: 'localhost',
+      dialect: 'mysql',
+      PORT:3306,
+    });
 
-async function connect() {
-  try {
-    await sequelize.authenticate();
-    console.log("Connection has been made.");
-  } catch (error) {
-    console.error("Unable to establish a connection to the database:", error);
-  }
-}
-
-connect();
-
+    
 module.exports = sequelize;
